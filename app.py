a816207b18e058ee8c3c0aa21eca972152aa739e
rsreddy <radha21@gmail.com>
2017-05-10 21:13:14 +0000

    initial shell for smilecommunity

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask, render_template, request
+import requests, json
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/')
 def index():
+    r = requests.get(app.config['URL'] + app.config['API_KEY'])
     return render_template(
-        'index.html')
+        'index.html',
+        articles=json.loads(r.text)['articles'])
+
+
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+
