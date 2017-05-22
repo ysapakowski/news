e915ee445676b8842f242112fa3a7e1e1064e28f
rsreddy <radhika.b.shah@gmail.com>
2017-05-21 23:34:35 -0500

    modified shell based on topics

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
 import os
-from flask import Flask, render_template, request
-import requests, json
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object('config')
 
 @app.route('/')
 def index():
-    r = requests.get(app.config['URL'] + app.config['API_KEY'])
     return render_template(
-        'index.html',
-        articles=json.loads(r.text)['articles'])
+        'index.html')
 
+@app.route('/environment')
+def environment():
+    return render_template(
+        'environment.html',
+        topic = 'environment'
+    )
 
+@app.route('/equality')
+def equality():
+    return render_template(
+        'equality.html',
+        topic = 'equality'
+    )
 
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+@app.route('/education')
+def education():
+    return render_template(
+        'education.html',
+        topic = 'education'
+    )
+
+@app.route('/feminism')
+def feminism():
+    return render_template(
+        'feminism.html',
+        topic = 'feminism'
+    )
+
+@app.route('/science')
+def science():
+    return render_template(
+        'science.html',
+        topic = 'science'
+    )
+app.run(host = os.getenv('IP','0.0.0.0'), port = int(os.getenv('PORT',8080)))
 
